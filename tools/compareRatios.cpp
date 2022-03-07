4e28df0adcd0088bd627860f599334c1d03f2cdc
wmhenrytemple <billyh@temple.edu>
2022-03-07 11:17:33 -0500

    blah

--- tools/compareRatios.cpp
+++ tools/compareRatios.cpp
@@ -1,16 +1,16 @@
 
 
-void compareRatios(string tgt="h",string angle="29", string mom="3p0",string spec="shms"){
+void compareRatios(string tgt="h",string angle="21", string mom="3p3",string spec="shms"){
   string kin=tgt+angle+"deg"+mom; 
   cout << "Kinematic is : "<<kin<<endl;
   Int_t DRAW=1;
   gROOT->ForceStyle();
-  TFile *fm=new TFile(Form("../ratiosOut/pass251/%s_ratios%s.root",spec.c_str(),kin.c_str()));
+  TFile *fm=new TFile(Form("../ratiosOut/pass303/%s_ratios%s.root",spec.c_str(),kin.c_str()));
   TH1F *hdd1=(TH1F*)fm->Get("hrd");
   hdd1->SetName("hdd1");
   hdd1->SetDirectory(0);
 
-  TFile *fm2=new TFile(Form("../ratiosOut/pass303/%s_ratios%s.root",spec.c_str(),kin.c_str()));
+  TFile *fm2=new TFile(Form("../ratiosOut/pass251save/%s_ratios%s.root",spec.c_str(),kin.c_str()));
   TH1F *hdd2=(TH1F*)fm2->Get("hrd");
   hdd2->SetName("hdd2");
   hdd2->SetDirectory(0);
