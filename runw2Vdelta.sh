df052d0b26dfd1b82efe1840cae3bdf4965e77c7
wmhenrytemple <billyh@temple.edu>
2022-06-02 12:55:13 -0400

    Cleanup

--- runw2Vdelta.sh
+++
@@ -1,8 +0,0 @@
-#!/Bin/bash
-
-root -b -q 'w2Vdelta.cpp("20")'
-root -b -q 'w2Vdelta.cpp("21")'
-root -b -q 'w2Vdelta.cpp("25")'
-root -b -q 'w2Vdelta.cpp("29")'
-root -b -q 'w2Vdelta.cpp("33")'
-root -b -q 'w2Vdelta.cpp("39")'
